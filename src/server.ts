31f3c7096e65844d07f22c69f402bc41550556ab
fekom <fekomninakis@gmail.com>
2026-03-07 23:48:11 -0300

    feat: basic setup for jitterbit test using fastify and prisma

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import Fastify from 'fastify'
+
+const app = Fastify({ logger: true })
+
+app.listen({ port: Number(process.env.PORT) || 3000, host: '0.0.0.0' }, (err) => {
+	if (err) {
+		app.log.error(err)
+		process.exit(1)
+	}
+})
